f90145a6b172cba536d34e29a90047da4e4e5fdc
Mike Hostetler <84222+mikehostetler@users.noreply.github.com>
2025-06-22 18:05:32 -0500

    chore: downgrade Elixir version requirement from 1.18 to 1.17 in mix.exs

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule JidoAction.MixProject do
     [
       app: :jido_action,
       version: "0.1.0",
-      elixir: "~> 1.18",
+      elixir: "~> 1.17",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
       elixirc_paths: elixirc_paths(Mix.env())
